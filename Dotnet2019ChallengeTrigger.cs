6a32fae1d3e1d7faa62d56bbf9ca5c5fc4a60b11
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2019-06-17 13:25:35 +0200

    Slim version

--- Dotnet2019ChallengeTrigger.cs
+++ Dotnet2019ChallengeTrigger.cs
@@ -1,42 +1,27 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using System.Net.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Microsoft.Devpmms
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
 {
-    public static class Dotnet2019ChallengeTrigger
+    HttpClient client = new HttpClient();
+
+    log.LogInformation("C# HTTP trigger function processed a request.");
+
+    var request = new HttpRequestMessage
     {
+        RequestUri = new Uri("https://httpsecreturl.azurewebsites.net/api/HttpSecretURLTrigger"),
+        Method = HttpMethod.Get,
+    };
+    var result = client.SendAsync(request).Result;
+    result.EnsureSuccessStatusCode();
 
-        static HttpClient client = new HttpClient();
+    var secretUrl = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-        [FunctionName("Dotnet2019ChallengeTrigger")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+    var htmlPage = "<html><head><meta http-equiv='refresh' content='3;url=" + secretUrl + "'></head><body>Please wait...</body></html>";
 
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri("https://httpsecreturl.azurewebsites.net/api/HttpSecretURLTrigger"),
-                Method = HttpMethod.Get,
-            };
-            var result = client.SendAsync(request).Result;
-            result.EnsureSuccessStatusCode();
-
-            var secretUrl = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            var htmlPage = "<html><head><meta http-equiv='refresh' content='3;url=" + secretUrl + "'></head><body>Please wait...</body></html>";
-
-            return new ContentResult { Content = htmlPage, ContentType = "text/html" };
-//            return  (ActionResult)new OkObjectResult(htmlPage);
-//            return  new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
-    }
+    return new ContentResult { Content = htmlPage, ContentType = "text/html" };
 }
